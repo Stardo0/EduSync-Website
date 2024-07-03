3ca32dc97df525a2ec37bb0f68047f8faa7e9013
Marc <marc.blattmann08@gmail.com>
2024-07-03 20:17:44 +0200

    Main

---
+++ app/layout.tsx
@@ -0,0 +1,41 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "@/components/ui/sonner";
+import { cn } from "@/lib/utils";
+import type { Metadata } from "next";
+import { Inter as FontSans } from "next/font/google";
+import "./globals.css";
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+export const metadata: Metadata = {
+  title: "Magic UI",
+  description: "The startup template from Magic UI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="light"
+          disableTransitionOnChange
+        >
+          {children}
+          <Toaster />
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/landing/hero-section.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { BorderBeam } from "@/components/magicui/border-beam";
+import TextShimmer from "@/components/magicui/text-shimmer";
+import { Button } from "@/components/ui/button";
+import { ArrowRightIcon } from "@radix-ui/react-icons";
+import { useInView } from "framer-motion";
+import { useRef } from "react";
+
+export default function HeroSection() {
+  const ref = useRef(null);
+  const inView = useInView(ref, { once: true, margin: "-100px" });
+  return (
+    <section
+      id="hero"
+      className="relative mx-auto mt-32 max-w-[80rem] px-6 text-center md:px-8"
+    >
+      <div className="backdrop-filter-[12px] inline-flex h-7 items-center justify-between rounded-full border border-white/5 bg-white/10 px-3 text-xs text-white dark:text-black transition-all ease-in hover:cursor-pointer hover:bg-white/20 group gap-1 translate-y-[-1rem] animate-fade-in opacity-0">
+        <TextShimmer className="inline-flex items-center justify-center">
+          <span>✨ Introducing EduSync Ai Learning</span>{" "}
+          <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+        </TextShimmer>
+      </div>
+      <h1 className="bg-gradient-to-br dark:from-white from-black from-30% dark:to-white/40 to-black/40 bg-clip-text py-6 text-5xl font-medium leading-none tracking-tighter text-transparent text-balance sm:text-6xl md:text-7xl lg:text-8xl translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:200ms]">
+        Learn Smarter
+        <br className="hidden md:block" /> with Edusync
+      </h1>
+      <p className="mb-12 text-lg tracking-tight text-gray-400 md:text-xl text-balance translate-y-[-1rem] animate-fade-in opacity-0 [--animation-delay:400ms]">
+        With a focus on simplicity and ease of use, EduSync grants you access to
+        <br className="hidden md:block" /> a Planner, Toutors, Quizzes and more...
+      </p>
+      <Button className="translate-y-[-1rem] animate-fade-in gap-1 rounded-lg text-white dark:text-black opacity-0 ease-in-out [--animation-delay:600ms]">
+        <span>Join Waitlist </span>
+        <ArrowRightIcon className="ml-1 size-4 transition-transform duration-300 ease-in-out group-hover:translate-x-1" />
+      </Button>
+      <div
+        ref={ref}
+        className="relative mt-[8rem] animate-fade-up opacity-0 [--animation-delay:400ms] [perspective:2000px] after:absolute after:inset-0 after:z-50 after:[background:linear-gradient(to_top,hsl(var(--background))_30%,transparent)]"
+      >
+      </div>
+    </section>
+  );
+}
